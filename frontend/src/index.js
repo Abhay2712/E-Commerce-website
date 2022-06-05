601384446679fe819736f470d8177f83960b49fe
Abhay2712 <abhaysharma754@gmail.com>
2022-06-06 03:06:41 +0530

    Added redux so that components can use the global state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './bootstrap.min.css';
 import './index.css';
+import {Provider} from 'react-redux';
+import store from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,88 +1,93 @@
-import React,{useState,useEffect} from 'react'
-import { useLocation } from "react-router-dom"
-import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup,Card,Button } from 'react-bootstrap';
-import Rating from '../components/Rating';
-import axios from 'axios';
-// import products from '../products';
+import React, { useEffect } from 'react'
 
-const ProductScreen = () => {
-    const [product,setProduct] = useState({});
-    let a = useLocation().pathname.split('/')[2];
-    useEffect(()=>{
-        
-        const fetchProduct = async () => {
-          const {data} = await axios.get('/api/products/'+a);
-          setProduct(data);
-        }
-        fetchProduct();
-      },[a])
-
-    // const product = products.find((p) => ('/product/' + p._id) === location);
-    // return <div>{product._id}</div>
-    return (
-        <>
-            <Link className='btn btn-dark my-3' to='/'>
-                Go Back
-            </Link>
-            <Row>
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid />
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Price: &#8377;{product.price}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Description: {product.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant='flush'>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>
-                                        Price: 
-                                    </Col>
-                                    <Col>
-                                        <strong>&#8377;{product.price}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>
-                                        Status: 
-                                    </Col>
-                                    <Col>
-                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Button className='btn-block' type='button' disabled={product.countInStock===0}>
-                                    Add to Cart
-                                </Button> 
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
 
 
-        </>
-    )
 
+const ProductScreen = ({ match }) => {
+  const dispatch = useDispatch()
+  let {id}= useParams();
+  const productDetails = useSelector((state) => state.productDetails)
+  const { loading, error, product } = productDetails
 
+  useEffect(() => {
+    dispatch(listProductDetails(id))
+  }, [id,dispatch, match])
+
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button
+                    className='btn-block'
+                    type='button'
+                    disabled={product.countInStock === 0}
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </>
+  )
 }
 
 export default ProductScreen
